7815f99454e34e07799d0343602cc2ea0caacedc
Lien <volleywang@163.com>
2017-07-04 01:03:20 +0800

    add generation code

--- module-db-jooq/src/main/resources/iam.mysql.sql
+++ module-db-jooq/src/main/resources/iam.mysql.sql
@@ -1,16 +1,24 @@
-DROP TABLE IF EXISTS `SYS_Authentication`;
-DROP TABLE IF EXISTS `INFO_User`;
+create database testdb;
 
-CREATE TABLE `SYS_Authentication` (
-    `id` int NOT NULL,
-    `auth_id` varchar(255) NOT NULL,
+CREATE USER name IDENTIFIED BY 'test';
+SET PASSWORD FOR name=PASSWORD('test');
+
+GRANT SELECT ON testdb.* TO test;
+
+
+DROP TABLE IF EXISTS `sys_authentication`;
+DROP TABLE IF EXISTS `info_user`;
+
+CREATE TABLE `sys_authentication` (
+    `auth_id` int NOT NULL,
+    `auth_code` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
 
-    CONSTRAINT pk_sys_authentication PRIMARY KEY (`id`)
+    CONSTRAINT pk_sys_authentication PRIMARY KEY (`auth_id`)
 );
 
-CREATE TABLE `INFO_User` (
-    `id` int NOT NULL,
+CREATE TABLE `info_user` (
+    `user_id` int NOT NULL,
     `user_name` varchar(255) NOT NULL,
     `nick_name` varchar(255) DEFAULT NULL,
     `first_name` varchar(255) DEFAULT NULL,
@@ -19,5 +27,5 @@ CREATE TABLE `INFO_User` (
     `mobile_number` varchar(255) DEFAULT NULL,
 
 
-    CONSTRAINT pk_info_user PRIMARY KEY (`id`)
+    CONSTRAINT pk_info_user PRIMARY KEY (`user_id`)
 );
